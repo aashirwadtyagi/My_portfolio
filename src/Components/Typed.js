66d99a97c55100130037babb548c9c75ee99c77c
Naman <110986742+officialDeveloper01@users.noreply.github.com>
2024-05-01 20:29:17 +0530

    Update Typed.js

--- src/Components/Typed.js
+++ src/Components/Typed.js
@@ -6,7 +6,7 @@ const Typed = () => {
     <div className="TypeEffect">
       <Typewriter
         options={{
-          strings: ['Software Developer', 'MERN Stack Developer'],
+          strings: ['Software Developer', 'FULL Stack Developer'],
           autoStart: true,
           loop: true,
           delay: 70,
